4b9f9b36eb5901793d56d34faac794759524e363
LRVT <21357789+l4rm4nd@users.noreply.github.com.>
2025-04-13 22:25:05 +0000

    Release v1.2.21

--- dist/python-3.12/pyarmor_runtime_000000/__init__.py
+++ dist/python-3.12/pyarmor_runtime_000000/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 9.1.3 (trial), 000000, 2025-04-13T19:52:31.470470
+# Pyarmor 9.1.3 (trial), 000000, 2025-04-13T22:24:24.943391
 from .pyarmor_runtime import __pyarmor__
--- dist/python-3.13/pyarmor_runtime_000000/__init__.py
+++ dist/python-3.13/pyarmor_runtime_000000/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 9.1.3 (trial), 000000, 2025-04-13T19:52:31.704034
+# Pyarmor 9.1.3 (trial), 000000, 2025-04-13T22:24:23.389160
 from .pyarmor_runtime import __pyarmor__
